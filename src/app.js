aa335ff2041b9b51010b1a2112a0bc644e685401
zzswang <zzswang@gmail.com>
2020-10-31 11:18:18 +0800

    chore: remove koa log

--- src/app.js
+++ src/app.js
@@ -6,14 +6,11 @@ import body from "koa-body";
 import compress from "koa-compress";
 import cors from "@koa/cors";
 import helmet from "koa-helmet";
-import koaLogger from "koa-logger";
-import koaPinoLogger from "koa-pino-logger";
 import Router from "koa-tree-router";
 import health from "@36node/koa-health";
 import openapi from "@36node/koa-openapi";
 
-import logger from "./lib/log";
-import { BASE, NODE_ENV } from "./config";
+import { BASE } from "./config";
 import { logService } from "./services";
 import pkg from "../package.json";
 import { errHandler } from "./middlewares";
@@ -25,16 +22,6 @@ const router = new Router({ prefix: BASE });
  * register services
  */
 logService.bind(router);
-
-/**
- * logger
- */
-if (NODE_ENV !== "production") {
-  // simple log under development
-  app.use(koaLogger());
-} else {
-  app.use(koaPinoLogger({ logger }));
-}
 
 /**
  * application
